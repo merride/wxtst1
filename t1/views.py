907d147d19284a190ea6169ac2424d1b71a88006
merride <hyfei1@hotmail.com>
2015-05-25 07:44:17 +0800

    201505250743

--- t1/views.py
+++ t1/views.py
@@ -34,14 +34,34 @@ def index(request):
     else:
         return HttpResponse('Hello World')
     
-def post(self, request):
-    if not self.validate(request):
-        raise PermissionDenied
- 
-    soup = BeautifulSoup(request.body)
-    if soup.msgtype:
-            # do something here ...
-            # and return the specified content for the request user.
- 
-    return HttpResponse('')
 
+
+"""def responseMsg(request):  
+    rawStr = smart_str(request.raw_post_data)  
+    #rawStr = smart_str(request.POST['XML'])  
+    msg = paraseMsgXml(ET.fromstring(rawStr))  
+      
+    queryStr = msg.get('Content','You have input nothing~')  
+  
+    '''raw_youdaoURL = "http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=%s&version=1.1&q=" % (YOUDAO_KEY_FROM,YOUDAO_KEY,YOUDAO_DOC_TYPE)     
+    youdaoURL = "%s%s" % (raw_youdaoURL,urllib2.quote(queryStr))  
+  
+    req = urllib2.Request(url=youdaoURL)  
+    result = urllib2.urlopen(req).read()'''  
+  
+    replyContent = paraseYouDaoXml(ET.fromstring(result))  
+  
+    return getReplyXml(msg,replyContent)  
+"""
+
+Class wxinterface(View)：
+    def __init__(self):
+        self.TOKEN='merride'
+    def post(self,request,*args,**kwargs):
+        xml_str=request.body
+        xml=ET.fromstring(xml_str)
+        content=xml.find('Content').text
+        msgType=xml.find('MsgType').text
+        fromUserName=xml.find('FromUserName').text
+        toUserName=xml.find('ToUserName').text
+        createTime=xml.find('CreateTime').text
