0fdbae7a137ee2bef4abf3cdd43a2b96eba27e8d
merride <hyfei1@hotmail.com>
2015-06-01 10:00:42 +0800

    61

--- wxtst1/urls.py
+++ wxtst1/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from t1.views import wxinterface
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wxtst1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^weixin/jiekou/$', wxinterface.as_view(),name='jiekou'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','t1.views.index')
 )
